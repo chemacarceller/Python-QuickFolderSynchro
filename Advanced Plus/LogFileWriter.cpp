34fffe32de308e217b559ab21ad31d733adc9879
jocarpe <114078928+chemacarceller@users.noreply.github.com>
2026-04-02 11:54:46 +0200

    Add files via upload

--- Advanced Plus/LogFileWriter.cpp
+++ Advanced Plus/LogFileWriter.cpp
@@ -1,6 +1,7 @@
 #include "LogFileWriter.h"
 
 #include <fstream>
+#include <chrono>
 
 // For using it in Python 
 #include <pybind11/pybind11.h>
@@ -8,68 +9,80 @@ namespace py = pybind11;
 
 
 // Log file name defined as constant
-static const char* LOG_FILENAME = "QuickFolderSynchroPRO.log";
+static const char* LOG_FILENAME = "QuickFolderSynchroAdvancedPlus.log";
 
 // Translates your internal enum values into human-readable text for your log file.
 // It is used to generate the string that will be stored with respect to the enum
 const char* levels[] = {"DEBUG", "INFO", "WARN", "ERROR", "FATAL"};
 
-// Constructor: Initializes the shared memory segment and the shared state
+// The Operating System reserves 1MB (1024×1024 bytes) of physical RAM, assigning it the name "LogFileWriterShm"
+// The segment object initializes the internal memory manager
+// If the segment already exists, it is simply assigned to the state variable.
 LogFileWriter::LogFileWriter() : segment(open_or_create, "LogFileWriterShm", 1024 * 1024) 
 {
-    // Buscamos o creamos el estado compartido
-    // El constructor de SharedLogState se llama solo la primera vez que se crea el segmento, luego se reutiliza
+    // Gets the state object or constructs it if it does not exist
     state = segment.find_or_construct<SharedLogState>("SharedState")(segment.get_segment_manager());
 
-    // Contador de referencias
+    // Increase the number of references to the shared memory, that is, the number of processes that have access to the shared memory.
     state->process_count++;
+    
+    // Assign the object to the singleton variable
+    singleton = this;
 }
 
 
-// MÉTODO CRÍTICO: Solo se debe llamar desde el proceso principal de Python
-// Este método inicia el worker thread que se encargará de procesar los logs en segundo plano
-void LogFileWriter::start_worker() {
-    if (!worker_thread.joinable()) {
-        worker_thread = std::thread(&LogFileWriter::process_logs, this);
-    }
+// I don't know why, but when a process finishes, it doesn't call the destructor.
+// I've made it so that when exiting a Python process, the freeze function is called and it works
+LogFileWriter::~LogFileWriter() { }
 
-    // Creamos el archivo vacío para poder ir añadiendo
-    std::ofstream file(LOG_FILENAME, std::ios::trunc);
-    if (file.is_open()) file.close();
-}
 
-LogFileWriter::~LogFileWriter() {
+// Method that replaces the destructor 
+void LogFileWriter::freeze() {
 
+    // Decrements references to shared memory
     state->process_count--;
 
-    if (state->process_count == 0) {
+    // If the number of references is zero
+    if (state) {
+        if (state->process_count == 0) {
+            try {
 
-try {
-        if (state) {
+                // We block it to ensure the thread receives the signal
+                {
+                    scoped_lock<interprocess_mutex> lock(state->queue_mutex);
+                    state->should_exit = true;
+                }
 
-            // 1. Bloqueamos para asegurar que el hilo recibe la señal
-            {
-                scoped_lock<interprocess_mutex> lock(state->queue_mutex);
-                state->should_exit = true;
+                // We woke up to the thread
+                state->cv.notify_all();
+            
+
+                // We're waiting for the thread. If you don't use join(), std::thread will terminate.
+                if (worker_thread.joinable()) {worker_thread.join(); }
+
+                // ONLY NOW do we erase the shared memory
+                segment.destroy<SharedLogState>("SharedState");
+                shared_memory_object::remove("LogFileWriterShm");
+
+            } catch (...) {
+                // You guarantee that, whatever happens inside the destructor (freeze function), the thread of execution will continue its exit path peacefully.
             }
-            // 2. Despertamos al hilo
-            state->cv.notify_all();
         }
-
-        // 3. ESPERAMOS al hilo. Si no haces join(), std::thread lanza 'terminate'
-        if (worker_thread.joinable()) {
-            worker_thread.join();
-        }
-
-        // 4. SOLO AHORA borramos la memoria compartida
-        segment.destroy<SharedLogState>("LogState");
-        shared_memory_object::remove("QuickFolderSynchro_SHM");
-
-    } catch (...) {
-        // Nunca permitas que una excepción salga del destructor
-    }
     }
 
+    // When we close, we also set the pointer to nullptr
+    if (singleton == this) singleton = nullptr;
+}
+
+
+// This method starts the worker thread that will process the logs in the background
+void LogFileWriter::start_worker() {
+
+    if (!worker_thread.joinable()) worker_thread = std::thread(&LogFileWriter::process_logs, this);
+
+    // We create the empty file so we can add to it, taking advantage of the fact that this method only runs in the main process.
+    std::ofstream file(LOG_FILENAME, std::ios::trunc);
+    if (file.is_open()) file.close();
 }
 
 
@@ -84,10 +97,13 @@ void LogFileWriter::_log_internal(LogLevel p_level, const std::string& p_msg, co
     // Their job is to prevent the system from wasting time processing messages that the user has decided to ignore.
     if ((int)p_level < state->min_level) return;
 
+    // We block the message queue to have exclusive access
     scoped_lock<interprocess_mutex> lock(state->queue_mutex);
         
-    // Creamos el LogEntry usando el manager de la memoria compartida
+    // We create the LogEntry using the shared memory manager
     LogEntry entry(segment.get_segment_manager());
+
+    // We save the information in the log input object
     entry.level = p_level;
     entry.message = p_msg.c_str();
     entry.timestamp = get_timestamp().c_str();
@@ -95,53 +111,61 @@ void LogFileWriter::_log_internal(LogLevel p_level, const std::string& p_msg, co
     entry.line = p_line;
     entry.isStdOutput = isStdOutput;
 
-    state->cv.notify_one(); 
+    // We save the log entry in the FIFO queue
     state->log_queue.push_back(entry);
+
+    // We wake up the execution thread so that it writes to the log file
+    state->cv.notify_one(); 
     
 }
 
 
-
 void LogFileWriter::process_logs() {
+
+    // Abrir el fichero de logs para añadir entradas
     std::ofstream file(LOG_FILENAME, std::ios::app);
 
     while (true) {
 
-        scoped_lock<interprocess_mutex> lock(state->queue_mutex);
-        
-        // Espera hasta que haya logs O debamos salir
-        state->cv.wait(lock, [this]{ 
-            return !state->log_queue.empty() || state->should_exit; 
-        });
+        std::string output;
 
-        // Procesar TODO lo que haya en la cola actualmente
-        while (!state->log_queue.empty()) {
-
-            LogEntry& entry = state->log_queue.front();
+        {
+            // We block the FIFO queue to have exclusive access
+            scoped_lock<interprocess_mutex> lock(state->queue_mutex);
             
+            // It goes to sleep if the queue is empty and it shouldn't leave, and it clears the message queue until it is woken up.
+            // Once woken up, if there are messages in the queue, it will block the queue again.
+            state->cv.wait(lock, [this]{ return !state->log_queue.empty() || state->should_exit;  });
+
+            // It is used to exit the infinite loop when you decide to exit with should_exit
+            if (state->should_exit && state->log_queue.empty()) break;
+
+            // We access the following log entry to write it to the file
+            LogEntry& entry = state->log_queue.front();
+
+            // We generate the string to display on the screen and in the file
             std::ostringstream ss;
             ss << "[" << entry.timestamp << "] [" << levels[entry.level] << "] "
                << "[" << entry.file << ":" << entry.line << "] " << entry.message;
-            std::string output = ss.str();
+            output = ss.str();
 
+            // is displayed on screen
             if (entry.level >= ERROR) std::cerr << output << std::endl;
             else if (entry.isStdOutput) std::cout << output << std::endl;
 
-            if (file.is_open()) {
-                file << output << std::endl;
-            }
-            
+            // We remove the entry from the FIFO queue
             state->log_queue.pop_front();
-        }
-        
-        // Forzar escritura en disco para que no se quede en el búfer de C++
-        if (file.is_open()) file.flush();
 
-        // SALIDA: Solo si should_exit es true Y la cola está realmente vacía
-        if (state->should_exit && state->log_queue.empty()) {
-            break;
+        }
+
+         // Everything is Writing to file
+        if (file.is_open()) {
+            file << output << std::endl;
+            file.flush();
         }
     }
+
+    // The file is closed
     if (file.is_open()) file.close();
 }
 
@@ -192,14 +216,16 @@ PYBIND11_MODULE(LogFileWriter, m) {
     // In Python, you will get an object like ogger = LogFileWriter.Writer.get_instance()
     .def_static("get_instance", &LogFileWriter::get_singleton, py::return_value_policy::reference)
     
-    //This binds the functions for logging
+    // Non-static methods available in Python
     .def("set_min_level", &LogFileWriter::set_min_level)
     .def("start_worker", &LogFileWriter::start_worker)
-    .def_static("LOG_DEBUG", [](std::string message, bool isStdOutput=true) { LOG_DEBUG(message, isStdOutput); }, py::arg("message"), py::arg("isStdOutput") = true)
-    .def_static("LOG_INFO", [](std::string message, bool isStdOutput=true) { LOG_INFO(message, isStdOutput); }, py::arg("message"), py::arg("isStdOutput") = true)
-    .def_static("LOG_WARN", [](std::string message, bool isStdOutput=true) { LOG_WARN(message, isStdOutput); }, py::arg("message"), py::arg("isStdOutput") = true)
-    .def_static("LOG_ERROR", [](std::string message, bool isStdOutput=true) { LOG_ERROR(message, isStdOutput); }, py::arg("message"), py::arg("isStdOutput") = true)
-    .def_static("LOG_FATAL", [](std::string message, bool isStdOutput=true) { LOG_FATAL(message, isStdOutput); }, py::arg("message"), py::arg("isStdOutput") = true);
+    .def("freeze", &LogFileWriter::freeze)
+    // Static methods available in Python; they call macros
+    .def_static("LOG_DEBUG", [](std::string message, const std::string& p_file, int p_line, bool isStdOutput=true) { LOG_DEBUG(message, p_file, p_line, isStdOutput); }, py::arg("message"), py::arg("p_file")=__FILE__, py::arg("p_line")=__LINE__, py::arg("isStdOutput") = true)
+    .def_static("LOG_INFO", [](std::string message, const std::string& p_file, int p_line, bool isStdOutput=true) { LOG_INFO(message, p_file, p_line, isStdOutput); }, py::arg("message"), py::arg("p_file")=__FILE__, py::arg("p_line")=__LINE__, py::arg("isStdOutput") = true)
+    .def_static("LOG_WARN", [](std::string message, const std::string& p_file, int p_line, bool isStdOutput=true) { LOG_WARN(message, p_file, p_line, isStdOutput); }, py::arg("message"), py::arg("p_file")=__FILE__, py::arg("p_line")=__LINE__, py::arg("isStdOutput") = true)
+    .def_static("LOG_ERROR", [](std::string message, const std::string& p_file, int p_line, bool isStdOutput=true) { LOG_ERROR(message, p_file, p_line, isStdOutput); }, py::arg("message"), py::arg("p_file")=__FILE__, py::arg("p_line")=__LINE__, py::arg("isStdOutput") = true)
+    .def_static("LOG_FATAL", [](std::string message, const std::string& p_file, int p_line, bool isStdOutput=true) { LOG_FATAL(message, p_file, p_line, isStdOutput); }, py::arg("message"), py::arg("p_file")=__FILE__, py::arg("p_line")=__LINE__, py::arg("isStdOutput") = true);
 
     // This binds the enum values
     py::enum_<LogFileWriter::LogLevel>(m, "LogLevel")
--- Advanced Plus/LogFileWriter.h
+++ Advanced Plus/LogFileWriter.h
@@ -1,6 +1,7 @@
 #ifndef LOG_FILE_WRITER_H
 #define LOG_FILE_WRITER_H
 
+// C++ library designed to simplify inter-process communication (IPC) and synchronization
 #include <boost/interprocess/managed_shared_memory.hpp>
 #include <boost/interprocess/containers/string.hpp>
 #include <boost/interprocess/containers/deque.hpp>
@@ -16,7 +17,8 @@
 
 using namespace boost::interprocess;
 
-// Allocators
+// An allocator is an object that manages how and where memory is reserved for your data.
+// Allocator for strings
 typedef allocator<char, managed_shared_memory::segment_manager> CharAlloc;
 typedef basic_string<char, std::char_traits<char>, CharAlloc> shared_string;
 
@@ -29,37 +31,50 @@ struct LogEntry {
     int line;
     bool isStdOutput;
 
-    // Constructor necesario para inicializar los strings en la memoria compartida
+    // Constructor needed to initialize strings in shared memory
     LogEntry(managed_shared_memory::segment_manager* sm) : message(sm), timestamp(sm), file(sm) {}
 };
 
-// Allocator para la cola de Logs
+// Allocator for the Logs queue
 typedef allocator<LogEntry, managed_shared_memory::segment_manager> LogEntryAlloc;
 typedef deque<LogEntry, LogEntryAlloc> SharedLogQueue;
 
+// Data structure that is stored in shared memory
 struct SharedLogState {
     SharedLogQueue log_queue;
     interprocess_mutex queue_mutex;
     interprocess_condition cv;
     std::atomic<int> min_level;
     std::atomic<bool> should_exit;
-    std::atomic<int> process_count;
+    std::atomic<int> process_count = 0;
 
     SharedLogState(managed_shared_memory::segment_manager* sm) : log_queue(sm), min_level(0), should_exit(false) { process_count=0; }
 };
 
+// This class is a singleton for managing a log file in a way that uses a shared memory area between different processes
+// Each process will have a singleton object, but the message queue is common to all processes
+// The thread that manages file writing is only configured in the main process, while in the rest of the processes, although it exists, it is not used for anything
 class LogFileWriter {
 
-    private:
-        SharedLogState* state; // Puntero al bloque en RAM compartida
+    private :
+
+        // Pointer to the data structure stored in shared memory
+        SharedLogState* state;
+
+        // Thread responsible for writing to the log file
         std::thread worker_thread;
+
+        // It represents the shared memory segment
         managed_shared_memory segment;
 
-        // Method that manages the writing of a log when it occurs
+        // Method that manages the writing of a log when it occurs. The thread of execution is the one that executes this method
         void process_logs();
 
-        // a method that provides us with the date professionally
+        // Method that provides us with the date
         std::string get_timestamp();
+
+        // Singleton pointer
+        static inline LogFileWriter* singleton = nullptr;
 
     public:
 
@@ -69,36 +84,49 @@ class LogFileWriter {
         LogFileWriter();
         ~LogFileWriter();
 
+        // Sets the minimum log level below which logs are not stored
         void set_min_level(int p_level);
+
+        // It is used to initialize the thread of execution to write to the file, only in the parent process; in the other processes this method is not launched, so the variable remains unused.
         void start_worker();
-        
-        static LogFileWriter* get_singleton() {
-            static LogFileWriter instance;
-            return &instance;
+
+        // It is used to free up resources; it is the replacement for the destructor that is not executed.
+        // It is called from all Python processes configured as atexit.register()
+        void freeze();
+
+        // It will give us one singleton instance per process
+        static LogFileWriter* get_singleton() { 
+            if (singleton == nullptr) {
+                singleton = new LogFileWriter(); 
+            }
+            
+            return singleton;
         }
         
+        // Function to write log entries to the FIFO list; each process executes its own method
         void _log_internal(LogLevel p_level, const std::string& p_msg, const std::string& p_file, int p_line, bool isStdOutput);
 };
 
+
 // C++ Helper Macros. To use in pure C++
-#define LOG_DEBUG(m, isStdOutput) \
+#define LOG_DEBUG(m, file, line, isStdOutput) \
     ((void)(LogFileWriter::get_singleton()->_log_internal( \
-        LogFileWriter::LogLevel::DEBUG, (m), __FILE__, __LINE__, isStdOutput)))
+        LogFileWriter::LogLevel::DEBUG, (m), file, line, isStdOutput)))
 
-#define LOG_INFO(m, isStdOutput) \
+#define LOG_INFO(m, file, line, isStdOutput) \
     ((void)(LogFileWriter::get_singleton()->_log_internal( \
-        LogFileWriter::LogLevel::INFO, (m), __FILE__, __LINE__, isStdOutput)))
+        LogFileWriter::LogLevel::INFO, (m), file, line, isStdOutput)))
 
-#define LOG_WARN(m, isStdOutput) \
+#define LOG_WARN(m, file, line, isStdOutput) \
     ((void)(LogFileWriter::get_singleton()->_log_internal( \
-        LogFileWriter::LogLevel::WARN, (m), __FILE__, __LINE__, isStdOutput)))
+        LogFileWriter::LogLevel::WARN, (m), file, line, isStdOutput)))
 
-#define LOG_ERROR(m, isStdOutput) \
+#define LOG_ERROR(m, file, line, isStdOutput) \
     ((void)(LogFileWriter::get_singleton()->_log_internal( \
-        LogFileWriter::LogLevel::ERROR, (m), __FILE__, __LINE__, isStdOutput)))
+        LogFileWriter::LogLevel::ERROR, (m), file, line, isStdOutput)))
 
-#define LOG_FATAL(m, isStdOutput) \
+#define LOG_FATAL(m, file, line, isStdOutput) \
     ((void)(LogFileWriter::get_singleton()->_log_internal( \
-        LogFileWriter::LogLevel::FATAL, (m), __FILE__, __LINE__, isStdOutput)))
+        LogFileWriter::LogLevel::FATAL, (m), file, line, isStdOutput)))
 
 #endif
